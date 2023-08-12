66ff2595d088e6f4502b1126b5bdc6e82b37736b
rosmis <rosmis123@gmail.com>
2023-08-12 14:11:18 +0200

    stabilized rotation svg on scroll + init scoll anchor links

--- src/js/work.js
+++ src/js/work.js
@@ -6,157 +6,188 @@ let panels = gsap.utils.toArray(".work"),
     observer = ScrollTrigger.normalizeScroll(true),
     scrollTween;
 
-let cards = gsap.utils.toArray(".card-wrapper")
-let cardWrappers = gsap.utils.toArray(".card-wrapper-transform")
+let cards = gsap.utils.toArray(".card-wrapper");
+let cardWrappers = gsap.utils.toArray(".card-wrapper-transform");
 
 // on touch devices, ignore touchstart events if there's an in-progress tween so that touch-scrolling doesn't interrupt and make it wonky
-document.addEventListener("touchstart", e => {
-  if (scrollTween) {
-    e.preventDefault();
-    e.stopImmediatePropagation();
-  }
-}, {capture: true, passive: false})
+document.addEventListener(
+    "touchstart",
+    (e) => {
+        if (scrollTween) {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+        }
+    },
+    { capture: true, passive: false }
+);
 
 panels.forEach((panel, i) => {
-  ScrollTrigger.create({
-    trigger: panel,
-    start: "top bottom",
-    end: "+=199%",
-    onToggle: self => self.isActive && !scrollTween && goToSection(i, self.direction)
-  });
+    ScrollTrigger.create({
+        trigger: panel,
+        start: "top bottom",
+        end: "+=199%",
+        onToggle: (self) =>
+            self.isActive && !scrollTween && goToSection(i, self.direction),
+    });
 });
 
+let counter = 0;
+
 function goToSection(i, direction, self) {
-  scrollTween = gsap.to(window, {
-    scrollTo: {y: i * innerHeight, autoKill: false},
-    onStart: () => {
-      observer.disable(); // for touch devices, as soon as we start forcing scroll it should stop any current touch-scrolling, so we just disable() and enable() the normalizeScroll observer
-      observer.enable();
+    scrollTween = gsap.to(window, {
+        scrollTo: { y: i * innerHeight, autoKill: false },
+        onStart: () => {
+            observer.disable(); // for touch devices, as soon as we start forcing scroll it should stop any current touch-scrolling, so we just disable() and enable() the normalizeScroll observer
+            observer.enable();
 
-      setCardAnimation(direction === 1 ? i - 1 : i, direction)
+            setCardAnimation(direction === 1 ? i - 1 : i, direction);
 
-      incrementArcRotation(cardWrappers.length, direction)
-      
-    },
-    duration: 0.8,
-    ease: Power1.easeInOut,
-    onComplete: () => scrollTween = null,
-    overwrite: true
-  });
+            counter++;
+
+            // this is to prevent the incrementArcRotation to trigger itself at when dom is mounted - didn't find a prettier way to do it
+            if (counter === 1) return;
+
+            incrementArcRotation(cardWrappers.length, direction);
+        },
+        duration: 0.8,
+        ease: Power1.easeInOut,
+        onComplete: () => (scrollTween = null),
+        overwrite: true,
+    });
 }
 
 // just in case the user forces the scroll to an inbetween spot (like a momentum scroll on a Mac that ends AFTER the scrollTo tween finishes):
 ScrollTrigger.create({
-  start: 0, 
-  end: "max",
-  snap: 1 / (panels.length - 1)
-})
+    start: 0,
+    end: "max",
+    snap: 1 / (panels.length - 1),
+});
 
 // ANIMATION CARD ON SCROLL
 
 function setCardAnimation(index, direction) {
-  // prevent gsap target error 
-  if(index === -1) return
+    // prevent gsap target error
+    if (index === -1) return;
 
-  const card_timeline1 = gsap.timeline({ paused: true });
-  const card_timeline2 = gsap.timeline({ paused: true });
-  
-  if(direction === 1) {
-    card_timeline1.to(cards[index], {
-      transform: "rotateX(0) rotateY(0) rotateZ(0)",
-      duration: 0.6,
-      ease: Power1.easeInOut,
-    });
+    const card_timeline1 = gsap.timeline({ paused: true });
+    const card_timeline2 = gsap.timeline({ paused: true });
 
-    card_timeline1.to(cardWrappers[index], {
-      opacity: 0,
-      duration: 0.6,
-      ease: Power1.easeInOut,
-    }, '-=0.6');
-    card_timeline1.play()
+    if (direction === 1) {
+        card_timeline1.to(cards[index], {
+            transform: "rotateX(0) rotateY(0) rotateZ(0)",
+            duration: 0.6,
+            ease: Power1.easeInOut,
+        });
+
+        card_timeline1.to(
+            cardWrappers[index],
+            {
+                opacity: 0,
+                duration: 0.6,
+                ease: Power1.easeInOut,
+            },
+            "-=0.6"
+        );
+        card_timeline1.play();
+
+        card_timeline2.to(cards[index + 1], {
+            transform: "rotateX(28deg) rotateY(-23deg) rotateZ(15deg)",
+            duration: 1,
+            ease: Power2.easeInOut,
+        });
+
+        card_timeline2.to(
+            cardWrappers[index + 1],
+            {
+                opacity: 1,
+                duration: 1,
+                ease: Power2.easeInOut,
+            },
+            "-=1"
+        );
+        card_timeline2.play();
+        return;
+    }
 
     card_timeline2.to(cards[index + 1], {
-      transform: "rotateX(28deg) rotateY(-23deg) rotateZ(15deg)",
-      duration: 1,
-      ease: Power2.easeInOut,
+        transform: "rotateX(0) rotateY(0) rotateZ(0)",
+        duration: 0.5,
+        ease: Power1.easeInOut,
     });
 
-    card_timeline2.to(cardWrappers[index + 1], {
-      opacity: 1,
-      duration: 1,
-      ease: Power2.easeInOut,
-    }, '-=1');
-    card_timeline2.play()
-    return
-  }
+    card_timeline2.to(
+        cardWrappers[index + 1],
+        {
+            opacity: 0,
+            duration: 0.5,
+            ease: Power1.easeInOut,
+        },
+        "-=0.5"
+    );
+    card_timeline2.play();
 
-  card_timeline2.to(cards[index + 1], {
-    transform: "rotateX(0) rotateY(0) rotateZ(0)",
-    duration: 0.5,
-    ease: Power1.easeInOut,
-  });
+    card_timeline1.to(cards[index], {
+        transform: "rotateX(28deg) rotateY(-23deg) rotateZ(15deg)",
+        duration: 0.6,
+        ease: Power1.easeInOut,
+    });
 
-  card_timeline2.to(cardWrappers[index + 1], {
-    opacity: 0,
-    duration: 0.5,
-    ease: Power1.easeInOut,
-  }, '-=0.5');
-  card_timeline2.play()
-
-  card_timeline1.to(cards[index], {
-    transform: "rotateX(28deg) rotateY(-23deg) rotateZ(15deg)",
-    duration: 0.6,
-    ease: Power1.easeInOut,
-  });
-
-  card_timeline1.to(cardWrappers[index], {
-    opacity: 1,
-    duration: 0.6,
-    ease: Power1.easeInOut,
-  }, '-=0.6');
-  card_timeline1.timeScale(0.8);
-  card_timeline1.play()
+    card_timeline1.to(
+        cardWrappers[index],
+        {
+            opacity: 1,
+            duration: 0.6,
+            ease: Power1.easeInOut,
+        },
+        "-=0.6"
+    );
+    card_timeline1.timeScale(0.8);
+    card_timeline1.play();
 }
 
 // SCROLLING ARC ANIMATION ON SCROLL
+document.addEventListener("DOMContentLoaded", () => console.log("loaaded"));
 
-const scrollArc = document.getElementById('circle-progress')
+const scrollArc = document.getElementById("circle-progress");
 
-const scrollingArcTimeline = gsap.timeline({paused: true})
-
+const scrollingArcTimeline = gsap.timeline({ paused: true });
 
 function incrementArcRotation(postsLength, direction) {
-  
-  const maxRadiusRotation = 280
-  const rotationRadiusPerIteration =  Math.floor(maxRadiusRotation / postsLength)
-  const arcPreviousMatrixTransform = window.getComputedStyle(scrollArc)['transform']
-  
- // Parse the rotation from the matrix
-  let rotateValue = 0;
-  const matrixPattern = /^matrix\((.+)\)$/;
-  const match = arcPreviousMatrixTransform.match(matrixPattern);
+    const maxRadiusRotation = 120;
+    const rotationRadiusPerIteration = Math.floor(
+        maxRadiusRotation / postsLength
+    );
+    const arcPreviousMatrixTransform =
+        window.getComputedStyle(scrollArc)["transform"];
 
-  if (match) {
-    const matrixValues = match[1].split(',').map(parseFloat);
-    if (matrixValues.length === 6) {
-      rotateValue = Math.atan2(matrixValues[1], matrixValues[0]) * (180 / Math.PI);
-      
-      // Adjust negative values to positive angles
-      if (rotateValue < 0) {
-        rotateValue += 360;
-      }
+    // Parse the rotation from the matrix
+    let rotateValue = 0;
+    const matrixPattern = /^matrix\((.+)\)$/;
+    const match = arcPreviousMatrixTransform.match(matrixPattern);
+
+    if (match) {
+        const matrixValues = match[1].split(",").map(parseFloat);
+        if (matrixValues.length === 6) {
+            rotateValue =
+                Math.atan2(matrixValues[1], matrixValues[0]) * (180 / Math.PI);
+
+            // Adjust negative values to positive angles
+            if (rotateValue < 0) {
+                rotateValue += 360;
+            }
+        }
     }
-  }
 
-  const updatedRotationRadius = direction === 1 ? rotateValue + rotationRadiusPerIteration : rotateValue - rotationRadiusPerIteration
+    const updatedRotationRadius =
+        direction === 1
+            ? rotateValue + rotationRadiusPerIteration
+            : rotateValue - rotationRadiusPerIteration;
 
+    scrollingArcTimeline.to(scrollArc, {
+        transform: `rotate(${updatedRotationRadius}deg)`,
+        duration: 1,
+        ease: Power1.easeInOut,
+    });
 
-  scrollingArcTimeline.to(scrollArc, {
-    transform: `translateY(-50%) rotate(${updatedRotationRadius}deg)`,
-    duration: 1,
-    ease: Power1.easeInOut,
-  });
-
-  scrollingArcTimeline.play()
-
+    scrollingArcTimeline.play();
 }
--- template-work.php
+++ template-work.php
@@ -15,15 +15,26 @@ get_header()
         <div class="background-wrapper" style="background-image: url(<?php echo get_template_directory_uri(). '/src/assets/salomon.png'?>)"></div>
     </div>
 
-    <div id="circle-progress">
-        <svg xmlns="http://www.w3.org/2000/svg" width="500" height="500" viewBox="0 0 785.693 823.191" id="rotate-circle">
-            <g id="Groupe_1932" data-name="Groupe 1932" transform="translate(-13.759 10.013)">
-                <path id="Ellipse_1712" data-name="Ellipse 1712" d="M347-2.5A347.877,347.877,0,0,1,537.47,53.913a2.5,2.5,0,1,1-2.728,4.19A342.877,342.877,0,0,0,347,2.5,2.5,2.5,0,0,1,344.5,0,2.5,2.5,0,0,1,347-2.5ZM76.793,562.23a2.5,2.5,0,0,1,1.947.93,345.054,345.054,0,0,0,161.1,111.348,2.5,2.5,0,0,1-1.554,4.752A350.055,350.055,0,0,1,74.849,566.3a2.5,2.5,0,0,1,1.944-4.07Z" transform="translate(373.485 -53.784) rotate(38)" fill="#c9961b"/>
-                <circle id="Ellipse_1715" data-name="Ellipse 1715" cx="30" cy="30" r="30" transform="translate(675.755 226.893) rotate(-35)" fill="#c1983a" opacity="0.099"/>
-                <circle id="Ellipse_1714" data-name="Ellipse 1714" cx="21" cy="21" r="21" transform="translate(688.289 229.103) rotate(-35)" fill="#c1983a" opacity="0.098"/>
-                <circle id="Ellipse_1713" data-name="Ellipse 1713" cx="12" cy="12" r="12" transform="translate(700.824 231.313) rotate(-35)" fill="#c9961b"/>
-            </g>
-        </svg>
+    <div class="circle-progress-wrapper">
+        <div id="circle-progress">
+            <svg xmlns="http://www.w3.org/2000/svg" width="500" height="500" viewBox="0 0 785.693 823.191" id="rotate-circle">
+                <g id="Groupe_1932" data-name="Groupe 1932" transform="translate(-13.759 10.013)">
+                    <path id="Ellipse_1712" data-name="Ellipse 1712" d="M347-2.5A347.877,347.877,0,0,1,537.47,53.913a2.5,2.5,0,1,1-2.728,4.19A342.877,342.877,0,0,0,347,2.5,2.5,2.5,0,0,1,344.5,0,2.5,2.5,0,0,1,347-2.5ZM76.793,562.23a2.5,2.5,0,0,1,1.947.93,345.054,345.054,0,0,0,161.1,111.348,2.5,2.5,0,0,1-1.554,4.752A350.055,350.055,0,0,1,74.849,566.3a2.5,2.5,0,0,1,1.944-4.07Z" transform="translate(373.485 -53.784) rotate(38)" fill="#c9961b"/>
+                    <circle id="Ellipse_1715" data-name="Ellipse 1715" cx="30" cy="30" r="30" transform="translate(675.755 226.893) rotate(-35)" fill="#c1983a" opacity="0.099"/>
+                    <circle id="Ellipse_1714" data-name="Ellipse 1714" cx="21" cy="21" r="21" transform="translate(688.289 229.103) rotate(-35)" fill="#c1983a" opacity="0.098"/>
+                    <circle id="Ellipse_1713" data-name="Ellipse 1713" cx="12" cy="12" r="12" transform="translate(700.824 231.313) rotate(-35)" fill="#c9961b"/>
+                </g>
+            </svg>
+        </div>
+
+        <div class="circled-image" style="background-image: url(<?php echo get_template_directory_uri(). '/src/assets/salomon.png'?>)"></div>
+
+        <div class="wrapper-circle-content">
+            <p class="anchor-work-link">Salomon</p>
+            <p class="anchor-work-link">Centre Pompidou</p>
+            <p class="anchor-work-link">musée d'orsay</p>
+        </div>
+
     </div>
 
     <div class="work">
