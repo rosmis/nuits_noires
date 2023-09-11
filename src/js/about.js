b5e3e1a1a58d237de83780920797f26919660c17
rosmis <rosmis123@gmail.com>
2023-09-11 17:26:14 -0500

    horizontal scroll in about section done

--- src/js/about.js
+++ src/js/about.js
@@ -1,27 +1,11 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const sections = gsap.utils.toArray(".panel");
-// const timelineWrapper = document.querySelector(".history-wrapper");
-
-// let scrollTween = gsap.to(sections, {
-//     x: (i) => -i * timelineWrapper.offsetWidth,
-//     // xPercent: -40 * (sections.length - 1),
-//     ease: "none",
-//     scrollTrigger: {
-//         trigger: timelineWrapper,
-//         pin: true,
-//         scrub: 0.1,
-//         markers: true,
-//         //snap: directionalSnap(1 / (sections.length - 1)),
-//         end: "+=3000",
-//         end: `+=${timelineWrapper.offsetWidth}`,
-//     },
-// });
 
 function GSAPHorizontalScroll() {
     let GSAPHorizontalScrollTL = gsap.timeline({
         scrollTrigger: {
-            trigger: "#timeline-wrapper",
+            trigger: ".history-wrapper",
             pin: true,
             start: "0% 0%",
             end:
@@ -34,88 +18,21 @@ function GSAPHorizontalScroll() {
         },
     });
 
-    GSAPHorizontalScrollTL.to(sections, {
+    const scrollTween = GSAPHorizontalScrollTL.to(sections, {
         xPercent: -100 * (sections.length - 1),
         ease: "none",
+    });
+
+    sections.forEach((item, index) => {
+        ScrollTrigger.create({
+            trigger: item,
+            containerAnimation: scrollTween,
+            toggleClass: "active",
+            start: "center 40%",
+        });
     });
 }
 
 window.onload = () => {
     GSAPHorizontalScroll();
 };
-
-// gsap.set(".box-1, .box-2", { y: 100 });
-// ScrollTrigger.defaults({ markers: { startColor: "white", endColor: "white" } });
-
-// red section
-// gsap.to(sections[0], {
-//   y: -130,
-//   duration: 2,
-//   ease: "elastic",
-//   scrollTrigger: {
-//     trigger: ".box-1",
-//     containerAnimation: scrollTween,
-//     start: "left center",
-//     toggleActions: "play none none reset",
-//     id: "1",
-//   }
-// });
-
-// // gray section
-// gsap.to(".box-2", {
-//   y: -120,
-//   backgroundColor: "#1e90ff",
-//   ease: "none",
-//   scrollTrigger: {
-//     trigger: ".box-2",
-//     containerAnimation: scrollTween,
-//     start: "center 80%",
-//     end: "center 20%",
-//     scrub: true,
-//     id: "2"
-//   }
-// });
-
-// // purple section
-// ScrollTrigger.create({
-//   trigger: ".box-3",
-//   containerAnimation: scrollTween,
-//   toggleClass: "active",
-//   start: "center 60%",
-//   id: "3"
-// });
-
-// sections.forEach((section, index) => {
-//     ScrollTrigger.create({
-//         trigger: section,
-//         containerAnimation: scrollTween,
-//         start: "center 65%",
-//         end: "center 51%",
-//         onEnter: () => console.log("enter"),
-//         onLeave: () => console.log("leave"),
-//         onEnterBack: () => console.log("enterBack"),
-//         onLeaveBack: () => console.log("leaveBack"),
-//         onToggle: (self) => console.log("active", self.isActive),
-//         id: index,
-//     });
-// });
-
-// // only show the relevant section's markers at any given time
-// gsap.set(
-//     ".gsap-marker-start, .gsap-marker-end, .gsap-marker-scroller-start, .gsap-marker-scroller-end",
-//     { autoAlpha: 0 }
-// );
-// ["red", "gray", "purple", "green"].forEach((triggerClass, i) => {
-//     ScrollTrigger.create({
-//         trigger: "." + triggerClass,
-//         containerAnimation: scrollTween,
-//         start: "left 30%",
-//         end: i === 3 ? "right right" : "right 30%",
-//         markers: false,
-//         onToggle: (self) =>
-//             gsap.to(".marker-" + (i + 1), {
-//                 duration: 0.25,
-//                 autoAlpha: self.isActive ? 1 : 0,
-//             }),
-//     });
-// });
--- template-about.php
+++ template-about.php
@@ -13,66 +13,69 @@ Template name: A PROPOS
         <h1><span></span> Notre histoire</h1>
 
         <div class="timeline-wrapper">
+
             <div class="panels-wrapper" id="timeline-wrapper">
                 <div class="panel timeline-item">
-                    <p class="timeline-item-title">2016</p>
-                    <p>Elodie et Jérémie se rencontrent à Lyon, et sont tous deux passionnés par le monde de l’art pluridisciplinaire.</p>
+                    <div class="timeline-item-content">
+                        <p class="timeline-item-title">2016</p>
+                        <p>Elodie et Jérémie se rencontrent à Lyon, et sont tous deux passionnés par le monde de l’art pluridisciplinaire.</p>
+                    </div>
+                        
+                    <p class="timeline-date">2016</p>
                 </div>
 
                 <div class="panel timeline-item">
-                    <p class="timeline-item-title">2017</p>
-                    <p>Ils se lancent dans les concerts dans le noir afin de montrer que la musique se vit par le sensoriel. Chaque date est sold out avec plus de 10 000 spectateurs au total. Ils ont reçu un public ému de 18 à 80 ans avec toujours cette envie d’accessibilité à tous.</p>
+                    <div class="timeline-item-content">
+                        <p class="timeline-item-title">2017</p>
+                        <p>Ils se lancent dans les concerts dans le noir afin de montrer que la musique se vit par le sensoriel. Chaque date est sold out avec plus de 10 000 spectateurs au total. Ils ont reçu un public ému de 18 à 80 ans avec toujours cette envie d’accessibilité à tous.</p>
+                    </div>
+                        
+                    <p class="timeline-date">2017</p>
                 </div>
 
                 <div class="panel timeline-item">
-                    <p class="timeline-item-title">2019</p>
-                    <p>La presse s’intéresse progressivement à leurs expériences et des marques leur confient des événements corporate pour faire vivre des expériences sensibles à leurs publics.
-                        C’est ainsi qu’est né Nuits Noires.
-                    </p>
+                    <div class="timeline-item-content">
+                        <p class="timeline-item-title">2019</p>
+                        <p>La presse s’intéresse progressivement à leurs expériences et des marques leur confient des événements corporate pour faire vivre des expériences sensibles à leurs publics.
+                            C’est ainsi qu’est né Nuits Noires.
+                        </p>
+                    </div>
+
+                    <p class="timeline-date">2019</p>
                 </div>
 
                 <div class="panel timeline-item">
-                    <p class="timeline-item-title">2020</p>
-                    <p>Le monde entier s’arrête, mais l’envie de créer est plus forte que tout. Ils décident de créer leur podcast « Ferme les yeux et regarde ». Après avoir reçu un grand nombre d’encouragements, ils décident de faire un virage à 180° en créant un studio de création sonore immersive dédié aux institutions culturelles, musées, territoires et marques.</p>
+                    <div class="timeline-item-content">
+                        <p class="timeline-item-title">2020</p>
+                        <p>Le monde entier s’arrête, mais l’envie de créer est plus forte que tout. Ils décident de créer leur podcast « Ferme les yeux et regarde ». Après avoir reçu un grand nombre d’encouragements, ils décident de faire un virage à 180° en créant un studio de création sonore immersive dédié aux institutions culturelles, musées, territoires et marques.</p>
+                    </div>
+
+                    <p class="timeline-date">2020</p>
                 </div>
 
                 <div class="panel timeline-item">
-                    <p class="timeline-item-title">2021</p>
-                    <p>La première institution à leur faire confiance 
-                        a été le Centre Pompidou et s’en suit de belles collaborations avec de prestigieux centres d’art 
-                        et musées comme le Muséum National d’Histoire Naturelle, le Palais de Tokyo, la Fondation Vuitton…
-                    </p>
+                    <div class="timeline-item-content">
+                        <p class="timeline-item-title">2021</p>
+                        <p>La première institution à leur faire confiance 
+                            a été le Centre Pompidou et s’en suit de belles collaborations avec de prestigieux centres d’art 
+                            et musées comme le Muséum National d’Histoire Naturelle, le Palais de Tokyo, la Fondation Vuitton…
+                        </p>
+                    </div>
+
+                    <p class="timeline-date">2021</p>
                 </div>
 
                 <div class="panel timeline-item">
-                    <p class="timeline-item-title">2023</p>
-                    <p>Aujourd’hui, Nuits Noires crée en exclusivité des contenus sonores immersifs et haut de gamme avec une sensibilité qui leur est propre. Avec pour mission de transmettre des savoirs en associant excellence et accessibilité. Et ainsi rendre visible, l’invisible.</p>
+                    <div class="timeline-item-content">
+                        <p class="timeline-item-title">2023</p>
+                        <p>Aujourd’hui, Nuits Noires crée en exclusivité des contenus sonores immersifs et haut de gamme avec une sensibilité qui leur est propre. Avec pour mission de transmettre des savoirs en associant excellence et accessibilité. Et ainsi rendre visible, l’invisible.</p>
+                    </div>
+
+                    <p class="timeline-date">2023</p>
                 </div>
-            </div>
 
 
-            <div class="date-wrapper">
-
-                <p class="date-panel">
-                    2016
-                </p>
-                <p class="date-panel">
-                    2016
-                </p>
-                <p class="date-panel">
-                    2016
-                </p>
-                <p class="date-panel">
-                    2016
-                </p>
-                <p class="date-panel">
-                    2016
-                </p>
-                <p class="date-panel">
-                    2016
-                </p>
-
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="263" height="120" viewBox="0 0 263 120" id="date-selector">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="263" height="80" viewBox="0 0 263 120" id="date-selector">
                     <defs>
                         <linearGradient id="linear-gradient" x1="0.5" x2="0.5" y2="1" gradientUnits="objectBoundingBox">
                         <stop offset="0" stop-color="#090909"/>
@@ -95,9 +98,34 @@ Template name: A PROPOS
                         <rect id="Rectangle_1724" data-name="Rectangle 1724" width="3" height="263" transform="translate(116 949.293) rotate(-90)" fill="url(#linear-gradient)"/>
                     </g>
                 </svg>
-
-
             </div>
+
+
+            <!-- <div class="timeline-wrapper"> -->
+
+                <!-- <p class="date-panel">
+                    2016
+                </p>
+                <p class="date-panel">
+                    2016
+                </p>
+                <p class="date-panel">
+                    2016
+                </p>
+                <p class="date-panel">
+                    2016
+                </p>
+                <p class="date-panel">
+                    2016
+                </p>
+                <p class="date-panel">
+                    2016
+                </p> -->
+
+                
+
+
+            <!-- </div> -->
         </div>
     </div>
 
