e33569e5bac178734abea1c79cb352fdf33cecbc
rosmis <rosmis123@gmail.com>
2023-10-21 19:02:08 +0200

    contact page refacto + V9 🥱

--- template-contact.php
+++ template-contact.php
@@ -46,12 +46,9 @@ get_header()
         </div>
 
         <div class="contact-img-grid">
-            <div style="background-image: url(https://nuitsnoires.com/wp-content/uploads/2023/10/nuits-noires-salon.webp)" class="contact-img"></div>
-            <div style="background-image: url(https://nuitsnoires.com/wp-content/uploads/2023/10/cuisine-nuitsnoires.webp)" class="contact-img"></div>
             <div style="background-image: url(https://nuitsnoires.com/wp-content/uploads/2023/10/liveroom-1-ps.webp)" class="contact-img"></div>
             <div style="background-image: url(https://nuitsnoires.com/wp-content/uploads/2023/10/dehors-1-nuitsnoires.webp)" class="contact-img"></div>
             <div style="background-image: url(https://nuitsnoires.com/wp-content/uploads/2023/10/regie-1-ps.webp)" class="contact-img"></div>
-            <div style="background-image: url(https://nuitsnoires.com/wp-content/uploads/2023/10/bureau-1-base-nuitsnoires.webp)" class="contact-img"></div>
         </div>
     </div>
 </section>
